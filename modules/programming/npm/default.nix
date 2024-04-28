e0db9b1c7bfb1adcc9375e462bf4843e633920de
Gabe Miles <gabrielmiles32@gmail.com>
2024-04-28 14:03:27 -0700

    4 current   2024-04-14 16:01:12  23.11.20240217.84d981b   Unknown                          *

--- modules/programming/npm/default.nix
+++ modules/programming/npm/default.nix
@@ -1,5 +1,10 @@
-{ ... }: {
-  imports = [
-    ./quartz.nix
-  ];
-}
+{ pkgs, ... }:
+  let
+    callPackage = pkgs.callPackage;
+  in {
+    nixpkgs.overlays = [(final: prev: {
+      npm = {
+        quartz = callPackage ./quartz.nix {};
+      };
+    })];
+  }
