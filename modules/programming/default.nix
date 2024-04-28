c9c489d12b97a377b932904187585f85421a14a1
Gabe Miles <gabrielmiles32@gmail.com>
2024-04-28 13:00:43 -0700

    4 current   2024-04-14 16:01:12  23.11.20240217.84d981b   Unknown                          *

--- modules/programming/default.nix
+++ modules/programming/default.nix
@@ -3,5 +3,11 @@
     ./python.nix
     ./kubernetes.nix
     ./ansible.nix
+    ./npm
   ];
+
+  home-manager.users.${config.user} = {
+    home.packages = (with npm; [
+      quartz
+    ]);
 }
---
+++ modules/programming/npm/quartz.nix
@@ -0,0 +1,21 @@
+{ lib, buildNpmPackage, fetchFromGitHub }:
+
+buildNpmPackage rec {
+  pname = "quartz";
+  version = "4.0.8";
+
+  src = fetchFromGithub {
+    owner = "jackyzha0";
+    repo = pname;
+    rev = "v${version}";
+    hast = "";
+  };
+
+  meta = with lib; {
+    description = "Quartz is a set of tools that helps you publish your digital garden and notes as a website for free. Quartz v4 features a from-the-ground rewrite focusing on end-user extensibility and ease-of-use.";
+    homepage = "https://quartz.jzhao.xyz/";
+    license = license.mit;
+  };
+
+}
+
